33ede3655cfe08381f815889c4673c1af632ef39
Hiro Ari <hiro0107@users.noreply.github.com>
2019-12-02 09:30:59 +0900

    Load markdown from backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,29 @@
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
+import {promisify} from 'util'
+
 const app = express()
 
-app.get('/', (req, res) => res.json({result: 'ok'}))
+const base = '/api/v1'
+const readfile = promisify(fs.readFile)
+
+if(process.env['ENV'] == 'dev') {
+  console.log("In development mode, allow cors")
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+}
+
+app.get(`${base}/story-map-as-markdown`, (req, res) => {
+  readfile(path.join(__dirname, '../story-map.md'))
+    .then(data => {
+      res
+        .contentType('text/plain')
+        .send(data)
+        .end()
+    })
+})
 app.listen(8080)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
+import useFetch from "react-fetch-hook";
 import './App.css';
 import {StoryMap} from './interface/StoryMap'
 import StoryBoard from './components/StoryBoard'
 import {markdown2storyMap} from './lib/md2storyMap'
 
 const App: React.FC = () => {
-  const md = `
-# Another Story Map Tool
-## Create a story map
-### You can write a story map by a story board
-### You can write a story map as a markdown
-## Show your stories
-### You can see a story map by a story board
-### You can see a story map as a markdown
-## Share your stories
-### You can share the stories by markdown.
-`
-  const storyMap: StoryMap = markdown2storyMap(md)
+  const { isLoading, data } = useFetch("http://localhost:8080/api/v1/story-map-as-markdown", {
+    formatter: (response) => response.text()
+  })
+
+  if(isLoading) {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+  const storyMap = markdown2storyMap(data ? data : "")
   return (
     <div className="App">
       <h1>{storyMap.title}</h1>
       <StoryBoard storyMap={storyMap}/>
     </div>
-  );
+  )
 }
 
 export default App;

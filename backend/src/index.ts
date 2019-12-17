d630ce97840aab791378c67ded31cd3137f6ee45
Hiro Ari <hiro0107@users.noreply.github.com>
2019-12-17 09:43:01 +0900

    Backend: Copy story-map.md if it does not exist

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import fs from 'fs'
+import fs, { symlinkSync, statSync } from 'fs'
 import path from 'path'
 import {promisify} from 'util'
 
@@ -7,6 +7,8 @@ const app = express()
 
 const base = '/api/v1'
 const readfile = promisify(fs.readFile)
+const stat = promisify(fs.stat)
+const copyFile = promisify(fs.copyFile)
 
 if(process.env['ENV'] == 'dev') {
   console.log("In development mode, allow cors")
@@ -16,14 +18,26 @@ if(process.env['ENV'] == 'dev') {
     next();
   });
 }
-app.use(express.static('../frontend/build'));
-app.get(`${base}/story-map-as-markdown`, (req, res) => {
-  readfile(path.join(__dirname, '../story-map.md'))
-    .then(data => {
-      res
-        .contentType('text/plain')
-        .send(data)
-        .end()
-    })
-})
-app.listen(8080)
+async function prepareMarkdownFile() {
+  const stats = await stat(path.join(__dirname, '../story-map.md')).catch(() => void 0)
+
+  // File does not exist, so File is needed to copy.
+  const needsCopy = typeof stats === "undefined"
+  await copyFile(path.join(__dirname, '../story-map-template.md'), path.join(__dirname, '../story-map.md'))
+}
+
+async function main() {
+  await prepareMarkdownFile()
+  app.use(express.static('../frontend/build'));
+  app.get(`${base}/story-map-as-markdown`, (req, res) => {
+    readfile(path.join(__dirname, '../story-map.md'))
+      .then(data => {
+        res
+          .contentType('text/plain')
+          .send(data)
+          .end()
+      })
+  })
+  app.listen(8080)
+}
+main()
